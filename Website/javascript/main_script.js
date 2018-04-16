ba63d6836ef7c860c13326a50d871aea56286f71
ChrisBlack51 <c3.black@connect.qut.edu.au>
2018-04-16 17:44:33 +1000

    Final Commit Design

--- Website/javascript/main_script.js
+++ Website/javascript/main_script.js
@@ -1,12 +1,19 @@
 // Google Map Api function
 function myMap() {
-            // Settings of the Map (Location and Zoom Magnification)
-                var mapProp= {
-                    center:new google.maps.LatLng(-27.477254,153.028064),
-                    zoom:12,
-                };
-            //Identify the div Id to be executed with
-                var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
-                }
-            //Source: Doesn't work externally. Investigation Required
-                src="https://maps.googleapis.com/maps/api/js?key=AIzaSyC_l2YWb5MNp1wgIe2XdbzLwyKH8vI5LhA&callback=myMap"
+    // Settings of the Map (Location and Zoom Magnification)
+    var mapProp = {
+        center: new google.maps.LatLng(-27.477254, 153.028064),
+        zoom: 12,
+    };
+    //Identify the div Id to be executed with
+    var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+}
+//Source: Doesn't work externally. Investigation Required
+src = "https://maps.googleapis.com/maps/api/js?key=AIzaSyC_l2YWb5MNp1wgIe2XdbzLwyKH8vI5LhA&callback=myMap"
+//Executes ratings drop-down
+function openFunction() {
+    document.getElementById("user_rating").style.display = "block";
+}
+function closeFunction() {
+    document.getElementById("user_rating").style.display = "none";
+}
