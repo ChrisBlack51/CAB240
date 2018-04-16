a7294b1bdf90690439ed61457f79bb866967f115
ChrisBlack51 <c3.black@connect.qut.edu.au>
2018-04-16 12:12:31 +1000

    Organizing/prepping for upload

---
+++ Website/javascript/main_script.js
@@ -0,0 +1,12 @@
+// Google Map Api function
+function myMap() {
+            // Settings of the Map (Location and Zoom Magnification)
+                var mapProp= {
+                    center:new google.maps.LatLng(-27.477254,153.028064),
+                    zoom:12,
+                };
+            //Identify the div Id to be executed with
+                var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+                }
+            //Source: Doesn't work externally. Investigation Required
+                src="https://maps.googleapis.com/maps/api/js?key=AIzaSyC_l2YWb5MNp1wgIe2XdbzLwyKH8vI5LhA&callback=myMap"
